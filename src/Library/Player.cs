d1ee6af22c1a0868fa85052a700333cb210a7f49
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-10-11 20:26:03 -0300

    solucion player v2

--- src/Library/Player.cs
+++ src/Library/Player.cs
@@ -3,29 +3,40 @@ using Library;
 
 public class Player
 {
-    public string  Name;
+    public string  Name { get; }
     public List<IPokemon> PokemonTeam { get; set;} =  new List<IPokemon> ();
-    IPokemon ActivePokemon;
+    public IPokemon ActivePokemon { get; private set; }
 
+    public Player(string name)
+    {
+        this.Name = name;
+    }
+    
     public IAction ChooseAction()
     {
-        // imprimir.movimientos();
-        // int x = Console.Read();
-        // //if
-        // return this.ActivePokemon.Moves[x - 1];
-        // //else
-        // this.ActivePokemon = Pokeball.ChangePokemon();
-        return null;
-        //podría llamar a una clase para imprimir en pantalla las opciones
+        // mostrar movimientos en pantalla, podría llamar a otra
+        // clase para imprimir en pantalla las opciones
+        int x = 0;//obtengo la selección del usuario, por ejemplo le pido un int
+        if (x < 5)
+        {
+            return this.ActivePokemon.Moves[x - 1];
+        }
+        else 
+        {
+            IAction action = new Pokeball();
+            return action;;
+        }
+        
     }
 
     public void AddToTeam(IPokemon pokemon)
     {
-
+        if(this.PokemonTeam.Count < 6)
+            this.PokemonTeam.Add(pokemon);
     }
 
-    public void ChoosePokemon()
+    public void ChangeActivePokemon(IPokemon pokemon)
     {
-
+        this.ActivePokemon = pokemon;
     }
 }
