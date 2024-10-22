b12555ab2792a89ad468f8f9305f9a9392c61912
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2024-10-22 17:49:28 -0300

    Plantilla Agregada

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,27 @@
-﻿using Library;
+﻿//--------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
 
-namespace Program;
+using System;
+using ClassLibrary;
 
-class Program
+namespace ConsoleApplication
 {
-    static void Main()
+    /// <summary>
+    /// Programa de consola de demostración.
+    /// </summary>
+    public static class Program
     {
-        
+        /// <summary>
+        /// Punto de entrada al programa principal.
+        /// </summary>
+        public static void Main()
+        {
+            var train = new Train();
+            train.StartEngines();
+            Console.WriteLine("Hello World!");
+        }
     }
 }
