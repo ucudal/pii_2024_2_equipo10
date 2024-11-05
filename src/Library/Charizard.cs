5e3ef33ec2bc8e583e89e10e5baa0f3431133b7d
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2024-11-05 14:07:25 -0300

    pokemon terminado

--- src/Library/Charizard.cs
+++ src/Library/Charizard.cs
@@ -2,54 +2,9 @@ namespace Library;
 
 public class Charizard : Pokemon
 {
-    public string Name { get; set; }
-    public List<IMove> Moves { get; private set; }
-    public List<Type> Types { get; private set; }
-    public int BaseLife { get; private set; }
-    public int BaseAttack { get; private set; }
-    public int BaseDefense { get; private set; }
-    public int CurrentLife { get; private set; }
-    public int CurrentAttack { get; private set; }
-    public int CurrentDefense { get; private set; }
-
-
-    public Charizard():base()
+    public Charizard():base(name: "Charizard", life: 80, type: Type.Fire, new DamageMove("string",true,100,75,Type.Fire))
     {
-        //Aplicamos Creator
-        this.Name = "Charizard";
-        this.BaseLife = 80;
-        this.BaseAttack = 109;
-        this.BaseDefense = 85;
-        this.CurrentLife = BaseLife;
-        this.CurrentAttack = BaseAttack;
-        this.CurrentDefense = BaseDefense;
-        this.Types = new List<Type>();
-        Type fire = Type.Fire;
-        this.Types.Add(fire);
-        // La lista de IMoves aplica LSP, ya que el pokemon puede tener movimientos de daño (DamageMove) o movimientos de buffeo (StatChangerMove)
-        // y el funcionamiento de la lista es el mismo.
-        this.Moves = new List<IMove>();
-        IMove flamethrower = new DamageMove("Flamethrower",false,100,90,fire);
-        this.Moves.Add(flamethrower);
+        
     }
-
-    public void RestoreBaseLife()
-    {
-        this.CurrentLife = BaseLife;
-    }
-
-    public void TakeDamage(int damage)
-    {
-        this.CurrentLife -= damage;
-    }
-
-    public void BuffAttack(int buff)
-    {
-        this.CurrentAttack *= buff;
-    }
-
-    public void BuffDefense(int buff)
-    {
-        this.CurrentDefense *= buff;
-    }
+    
 }
