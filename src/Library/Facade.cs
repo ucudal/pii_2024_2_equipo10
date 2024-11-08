7d5c1677794232687f94fb6a78934927670d3960
Ines Barral <barral.mariaines@gmail.com>
2024-11-08 16:58:44 -0300

    Documentacion fachada

--- src/Library/Facade.cs
+++ src/Library/Facade.cs
@@ -2,13 +2,30 @@ using System.Runtime.CompilerServices;
 
 namespace Library;
 
+
+/// <summary>
+/// La clase Facade proporciona una interfaz simplificada para interactuar con el sistema de gestión de batallas Pokemon.
+/// Incluye funcionalidades para gestionar listas de espera, partidas en curso, ataques, estados de juego, y mucho más.
+/// </summary>
 public static class Facade
 {
+    /// <summary>
+    /// Lista de espera para jugadores que aún no están en una partida.
+    /// </summary>
     private static WaitingList WaitingList { get; } = new WaitingList();
-
+    
+    /// <summary>
+    /// Lista de partidas en curso.
+    /// </summary>
     public static GameList GameList{ get; } = new GameList();
     
-    // historia de usuario 2
+    /// <summary>
+    /// Historia de usuario 2:
+    /// Muestra los ataques disponibles del Pokemon activo de un jugador.
+    /// </summary>
+    /// <param name="playerName">Nombre del jugador activo.</param>
+    /// <returns>Una lista de los ataques del Pokémon activo o un mensaje de error en
+    /// caso de que el jugador no exista.</returns>
     public static string ShowAtacks(string playerName)
     {
         
@@ -25,8 +42,15 @@ public static class Facade
 
         return result;
     }
-
-    // historia de usuario 3
+    
+    /// <summary>
+    /// Historia de usuario 3:
+    /// Muestra los puntos de vida (HP) de los Pokemon de un jugador.
+    /// </summary>
+    /// <param name="playerName">Nombre del jugador.</param>
+    /// <param name="playerToCheckName">Nombre del jugador cuya lista de Pokemon se va a comprobar (opcional). Si es
+    /// <c>null</c> hace referencia al propio jugador. Si no, hace referencia a otro.</param>
+    /// <returns>Una lista de los Pokemon y sus HP o un mensaje de error.</returns>
     public static string ShowPokemonsHP(string playerName, string playerToCheckName = null)
     {
         Player player = GameList.FindPlayerByName(playerName);
@@ -54,7 +78,12 @@ public static class Facade
         }
     }
 
-    //Historia de usuario 5
+    /// <summary>
+    /// Historia de usuario 5:
+    /// Comprueba si es el turno de un jugador y muestra las opciones disponibles.
+    /// </summary>
+    /// <param name="playerName">Nombre del jugador.</param>
+    /// <returns>Mensaje indicando si es o no su turno, junto con las opciones.</returns>
     public static string CheckTurn(string playerName)
     {
         Player player = GameList.FindPlayerByName(playerName);
@@ -73,7 +102,13 @@ public static class Facade
         return null;
     }
     
-    //Historia de usuario 6
+    /// <summary>
+    /// Historia de usuario 6:
+    /// Comprueba el estado de una partida y determina si continúa o hay un ganador.
+    /// </summary>
+    /// <param name="game">La partida actual.</param>
+    /// <returns><c>"Próximo turno"</c> en caso de que la partida siga o un <c>string</c> conteniendo el
+    /// ganador y el perdedor.</returns>
     public static string ChckGameStatus(Game game)
     {
         if (game.GameStatus())
@@ -83,7 +118,15 @@ public static class Facade
         return game.Winner();
     }
     
-    //Historia de usuario 8
+    /// <summary>
+    /// Historia de usuario 8
+    /// Permite a un jugador usar un item en un Pokemon.
+    /// </summary>
+    /// <param name="playerName">Nombre del jugador.</param>
+    /// <param name="item">Nombre del item a usar.</param>
+    /// <param name="pokemon">Nombre del Pokemon objetivo.</param>
+    /// <returns>Resultado del uso del item.</returns>
+
     public static string UseAnItem(string playerName, string item, string pokemon)
     {
         Player player = GameList.FindPlayerByName(playerName);
@@ -103,7 +146,12 @@ public static class Facade
     }
     
     
-    // historia de usuario 9
+    /// <summary>
+    /// Historia de usuario 9:
+    /// Agrega un jugador a la lista de espera.
+    /// </summary>
+    /// <param name="playerName">Nombre del jugador.</param>
+    /// <returns>Mensaje indicando si el jugador fue agregado o ya estaba en la lista.</returns>
     public static string AddPlayerToWaitingList(string playerName)
     {
         if (WaitingList.AddPlayer(playerName))
@@ -111,13 +159,24 @@ public static class Facade
         return $"{playerName} ya está en la lista de espera";
     }
     
+    /// <summary>
+    /// Historia de usuario 9.1:
+    /// Remueve un jugador de la lista de espera.
+    /// </summary>
+    /// <param name="playerName">Nombre del jugador.</param>
+    /// <returns>Mensaje indicando si el jugador fue removido o no estaba en la lista.</returns>
     public static string RemovePlayerFromWaitingList(string playerName)
     {
         if (WaitingList.RemovePlayer(playerName))
             return $"{playerName} removido de la lista de espera";
         return $"{playerName} no está en la lista de espera";
     }
-    //historia de usuario 10
+    
+    /// <summary>
+    /// Historia de usuario 10
+    /// Muestra todos los jugadores actualmente en la lista de espera.
+    /// </summary>
+    /// <returns>Lista de jugadores en espera o un mensaje indicando que no hay nadie esperando.</returns>
     public static string GetAllPlayersWaiting()
     {
         if (WaitingList.Count == 0)
@@ -133,7 +192,16 @@ public static class Facade
         
         return result;
     }
-    //historia de usuario 11
+    
+    //
+    /// <summary>
+    /// Historia de usuario 11:
+    /// Crea una nueva partida entre dos jugadores, quitándolos de la lista de espera y agregando la partida a la lista de
+    /// juegos activos.
+    /// </summary>
+    /// <param name="playerName">Nombre del primer jugador.</param>
+    /// <param name="opponentName">Nombre del oponente.</param>
+    /// <returns>Mensaje confirmando el inicio de la partida entre ambos jugadores.</returns>
     private static string CreateGame(string playerName, string opponentName)
     {
         Player player = WaitingList.FindPlayerByName(playerName);
@@ -144,6 +212,12 @@ public static class Facade
         return $"Comienza {playerName} vs {opponentName}";
     }
     
+    /// <summary>
+    /// Inicia una batalla entre dos jugadores, eligiendo un oponente específico o con el primer oponente disponible.
+    /// </summary>
+    /// <param name="playerName">Nombre del jugador que inicia la batalla.</param>
+    /// <param name="opponentName">Nombre del oponente (opcional).</param>
+    /// <returns> <c>string</c> indicando si la batalla comenzó o si hubo algún error.</returns>
     public static string StartBattle(string playerName, string? opponentName)
     {
         Player? opponent;
@@ -174,8 +248,13 @@ public static class Facade
         }
     }
     
-    // Historia 1
-    
+    /// <summary>
+    /// Historia 1:
+    /// Permite a un jugador agregar un Pokemon al equipo desde el catálogo.
+    /// </summary>
+    /// <param name="playerName">Nombre del jugador.</param>
+    /// <param name="cPokemon">Nombre del Pokemon que se quiere añadir al equipo.</param>
+    /// <returns>Mensaje indicando si el Pokemon fue añadido o si hubo un error.</returns>
     public static string ChooseTeam(string playerName, string cPokemon)
     {
         PokemonCatalogue.SetCatalogue();
@@ -202,6 +281,10 @@ public static class Facade
         return $"El pokemon {cPokemon} no fue encontrado";
     }
 
+    /// <summary>
+    /// Muestra el catálogo de Pokemon disponibles.
+    /// </summary>
+    /// <returns>Lista de Pokemon en el catálogo.</returns>
     public static string ShowCatalogue()
     {
         PokemonCatalogue.SetCatalogue();
