816458736c9f85506d5053422b1e2e2ae6cf1080
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-11-12 16:29:53 -0300

    surrenderCommand

---
+++ src/Library/Commands/SurrenderCommand.cs
@@ -0,0 +1,27 @@
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Library;
+
+namespace Library.Commands;
+
+/// <summary>
+/// Esta clase implementa el comando 'surrender' del bot.
+/// </summary>
+public class SurrenderCommand : ModuleBase<SocketCommandContext>
+{
+    /// <summary>
+    /// Termina la partida en curso dandole la victoria al oponente.
+    /// </summary>
+    [Command("changepokemon")]
+    [Summary(
+        """
+        Termina la partida en curso dandole la vicotria al oponente.
+        Este comando solo puede ser utilizado por un jugador que esté en una partida..
+        """)]
+    public async Task ExecuteAsync()
+    {
+        
+    }
+
+}
