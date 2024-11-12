781b20982cf398e2fd5eaf3175461e65749f6722
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-11-12 16:54:47 -0300

    ahora si todas las clases de comandos prontas

---
+++ src/Library/Commands/CheckTurnCommand.cs
@@ -0,0 +1,27 @@
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Library;
+
+namespace Library.Commands;
+
+/// <summary>
+/// Esta clase implementa el comando 'checkturn' del bot.
+/// </summary>
+public class CheckTurnCommand : ModuleBase<SocketCommandContext>
+{
+    /// <summary>
+    /// Devuelve de quien es el turno.
+    /// </summary>
+    [Command("checkturn")]
+    [Summary(
+        """
+        Devuelve de quien es el turno.
+        Se debe estar en partida para utilizar este comando.
+        """)]
+    public async Task ExecuteAsync()
+    {
+        
+    }
+
+}
