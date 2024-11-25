ea11e041ccd5d33a47b140ba62dc07bddc97e8bb
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-11-25 14:20:39 -0300

    ahora lo cubre todo

--- test/LibraryTests/GameTest.cs
+++ test/LibraryTests/GameTest.cs
@@ -28,18 +28,33 @@ public class GameTest
     }
 
     [Test]  //Test para verificar que se imprima correctamente el mensaje final ganador.
-    public void TestMensajeGanador()
+    public void TestWinnerPlayer1()
     {
         Player player1 = new Player("Player 1");
         Player player2 = new Player("Player 2");
-        Game game = new Game(player1, player2, new StrategyRandomStartingPlayer());
+        Game game = new Game(player1, player2, new StrategyPlayerOneStart());
         Charizard charizard= new Charizard();
         Gengar gengar = new Gengar();
-        player1.SetActivePokemon(charizard);
-        player2.SetActivePokemon(gengar);
+        player1.AddToTeam(charizard);
+        player2.AddToTeam(gengar);
         Attack attack = new Attack("kamehameha", Library.Type.Electric, 1, 1000);
         game.ExecuteAttack(attack);
-        Assert.That(game.Winner(), Is.EqualTo($"Ganador: Player 1. Perdedor: Player 2"));
+        Assert.That(game.Winner(), Is.EqualTo($"\nGanador: Player 1. Perdedor: Player 2"));
 
+    }
+
+    [Test]
+    public void TestWinnerPlayer2()
+    {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Game game = new Game(player1, player2, new StrategyPlayerTwoStart());
+        Charizard charizard= new Charizard();
+        Gengar gengar = new Gengar();
+        player1.AddToTeam(charizard);
+        player2.AddToTeam(gengar);
+        Attack attack = new Attack("kamehameha", Library.Type.Electric, 1, 1000);
+        game.ExecuteAttack(attack);
+        Assert.That(game.Winner(), Is.EqualTo($"\nGanador: Player 2. Perdedor: Player 1"));
     }
 }
